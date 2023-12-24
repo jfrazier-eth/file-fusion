96e0d32b939e623aaa24a39747d24447fdbf2bce
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-12-24 17:23:01 -0600

    removed unnecessary dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -122,7 +122,6 @@ export default function Home() {
               },
             };
 
-            console.log(`Saving message`, message);
             buffersMutation.mutate(message);
           }}
         />
